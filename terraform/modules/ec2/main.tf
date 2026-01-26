7140f91d5451f1e319da1bb0c917b529d628c02b
Rizeso <kacper_gruszczynski@outlook.com>
2026-01-26 22:02:02 +0100

    feat(ec2):Add bastion to public subnet

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,11 +1,27 @@
-resource "aws_instance" "ec2" {
+resource "aws_instance" "bastion" {
   ami                         = var.ami
   instance_type               = var.instance_type
-  subnet_id                   = var.subnet_id
-  vpc_security_group_ids      = []
+  subnet_id                   = var.pub_subnet_id
+  key_name = aws_key_pair.public_key.key_name
+  vpc_security_group_ids      = [
+  aws_security_group.sg_bastion.id
+  ]
   associate_public_ip_address = true
 
   tags = {
-    Name = "public-ec2"
+    Name = "bastion"
+  }
+}
+resource "aws_instance" "ec2" {
+  ami = var.ami
+  instance_type = var.instance_type
+  subnet_id = var.priv_subnet_id
+  key_name = aws_key_pair.public_key.key_name
+  vpc_security_group_ids = [
+  aws_security_group.sg_priv_ec2.id
+  ]
+
+  tags = {
+    Name = "priv-ec2"
   }
 }
