23fd090d0de5da6e1f3ec2d2e5839e91c5e9a17e
Rizeso <kacper_gruszczynski@outlook.com>
2026-01-26 22:02:50 +0100

    feat(ec2):Add Security Groups for bastion and private ec2

--- terraform/modules/ec2/sg_priv_ec2.tf
+++ terraform/modules/ec2/sg_priv_ec2.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "sg_priv_ec2" {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
