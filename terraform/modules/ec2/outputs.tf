b575539098fe16823e27c3e85d5bc719de14e6cc
Rizeso <kacper_gruszczynski@outlook.com>
2026-01-26 22:05:34 +0100

    feat(ec2): Add instances IP outputs

--- terraform/modules/ec2/outputs.tf
+++ terraform/modules/ec2/outputs.tf
@@ -1,6 +1,9 @@
 output "instance_id" {
   value = aws_instance.ec2.id
 }
-output "public_ec2_ip" {
-  value = aws_instance.ec2.public_ip
+output "bastion_ec2_ip" {
+  value = aws_instance.bastion.public_ip
+}
+output "private_ec2_ip" {
+  value = aws_instance.ec2.private_ip
 }
