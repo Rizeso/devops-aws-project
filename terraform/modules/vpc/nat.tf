b28ca55e81267112f0400d0537226de3e643231f
Rizeso <kacper_gruszczynski@outlook.com>
2026-01-26 22:13:09 +0100

    chore(vpc): Add inline routes, add depend_on in nat

--- terraform/modules/vpc/nat.tf
+++ terraform/modules/vpc/nat.tf
@@ -20,10 +20,6 @@ resource "aws_nat_gateway" "nat_gw" {
     Name = "devops-nat-gw"
   }
 
-<<<<<<< HEAD
   #Tworzenie NAT dopiero, gdy istnieje IGW
-=======
-  #Tworzenie NAT wtedy, gdy IGW istnieje
->>>>>>> bf47b8b (Before terraform fmt)
   depends_on = [aws_internet_gateway.gateway]
 }
