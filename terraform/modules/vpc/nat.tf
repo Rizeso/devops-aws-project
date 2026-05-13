dca0e8402ea3c65fcc2375beb9f3e06b0b628388
Rizeso <kacper_gruszczynski@outlook.com>
2026-05-13 22:20:26 +0200

    feat(vpc): add public and private subnets with NAT gateways for AZ-b

--- terraform/modules/vpc/nat.tf
+++ terraform/modules/vpc/nat.tf
@@ -1,23 +1,49 @@
-################
-## Elastic IP ##
-################
-resource "aws_eip" "nat_eip" {
+#----------------------#
+# NAT gateway for AZ-A #
+#----------------------#
+
+# Elastic IP
+resource "aws_eip" "nat_eip_a" {
   domain = "vpc"
 
   tags = {
-    Name = "devops-nat-gw-eip"
+    Name = "devops-nat-gw-eip_a"
   }
 
 }
-#################
-## NAT gateway ##
-#################
-resource "aws_nat_gateway" "nat_gw" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnet.id
+# NAT
+resource "aws_nat_gateway" "nat_gw_a" {
+  allocation_id = aws_eip.nat_eip_a.id
+  subnet_id     = aws_subnet.public_subnet_a.id
 
   tags = {
-    Name = "devops-nat-gw"
+    Name = "devops-nat-gw_a"
+  }
+
+  #Tworzenie NAT dopiero, gdy istnieje IGW
+  depends_on = [aws_internet_gateway.gateway]
+}
+
+#----------------------#
+# NAT gateway for AZ-B #
+#----------------------#
+
+# Elastic IP
+resource "aws_eip" "nat_eip_b" {
+  domain = "vpc"
+
+  tags = {
+    Name = "devops-nat-gw-eip_b"
+  }
+
+}
+# NAT
+resource "aws_nat_gateway" "nat_gw_b" {
+  allocation_id = aws_eip.nat_eip_b.id
+  subnet_id     = aws_subnet.public_subnet_b.id
+
+  tags = {
+    Name = "devops-nat-gw_b"
   }
 
   #Tworzenie NAT dopiero, gdy istnieje IGW
